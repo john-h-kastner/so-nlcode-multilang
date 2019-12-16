45e61db3a6b4d0e9d42695a354e1aea703b3db24
John Kastner <john.h.kastner@gmail.com>
2019-12-15 19:54:10 -0500

    Add some basic parallel processing to speed things up

--- parseXml.py
+++ parseXml.py
@@ -2,10 +2,15 @@ from bs4 import BeautifulSoup
 import ast
 import re
 import json
+import astor
+import html
+from multiprocessing import Pool
 #from csharp.title_filtering.SVM import SVM
 
 params = {
+#    "langXmlFile" : "/scratch0/python_en.xml",
     "langXmlFile" : "Posts.python.xml",
+#    "xmlFile" : "/scratch0/answers_en_nonzero.xml",
     "xmlFile" : "Posts.answers.xml",
     "outputFile" : "python_all.json"}
 
@@ -21,36 +26,54 @@ params = {
 # s.train("../csharp/title_filtering/balanced/pos_train.txt", "../csharp/title_filtering/balanced/neg_train.txt")
 # s.test("../csharp/title_filtering/balanced/pos_test.txt", "../csharp/title_filtering/balanced/neg_test.txt")
 
-# Two pass algorithm
-acceptedAnswers = {}
-
 # First pass. Get the posts tagged with C#. Filter the input using a grep on c# so that this is faster
+accepted_answer_re = re.compile(r"AcceptedAnswerId=\"(\d+)\"")
+id_re = re.compile(r"Id=\"(\d+)\"")
+title_re = re.compile(r"Title=\"([^\"]+)\"")
+tags_re = re.compile(r"Tags=\"([^\"]+)\"")
+body_re = re.compile(r"Body=\"([^\"]+)\"")
+def process_question(line):
+  acceptedanswerid = accepted_answer_re.search(line)
+  if acceptedanswerid is not None and "python" in tags_re.search(line).group(1):
+    return (int(acceptedanswerid.group(1)), {"id": int(id_re.search(line).group(1)), "title": title_re.search(line).group(1)})
+  else:
+    return None
+
+
+print('finding tagged posts')
 with open(params["langXmlFile"], 'r') as f:
-  for line in f:
-    y = BeautifulSoup(line, 'html.parser').row
-    if y.get("acceptedanswerid") is not None and "python" in y["tags"]:
-        acceptedAnswers[int(y["acceptedanswerid"])] = {"id": int(y["id"]), "title": y["title"] }
+  pool = Pool(processes=16)
+  acceptedAnswers = dict([p for p in pool.map(process_question, [l for l in f]) if p is not None])
+
+print('finding accepted answers')
+
+def find_answers(line):
+  qid = int(id_re.search(line).group(1))
+  if qid in acceptedAnswers:
+    return (qid, body_re.search(line).group(1))
+  else:
+    return None
 
 # Pass 2, find the corresponding accepted answer
 with open(params["xmlFile"], 'r') as f:
-  for line in f:
-    # Find the first attribute enclosed in "" It should be the Id
-    id1 = line.find("\"")              
-    id2 = line.find("\"", id1 + 1)
-    qid = int(line[(id1 + 1):id2])
-    if qid in acceptedAnswers:
-      y = BeautifulSoup(line, 'html.parser').row
-      acceptedAnswers[qid]["code"] = y["body"]
+  pool = Pool(processes=16)
+  codes = [p for p in pool.map(find_answers, [l for l in f]) if p is not None]
+  for qid,  code in codes:
+      acceptedAnswers[qid]["code"] = code
 
-code_pairs = []
-for rid in acceptedAnswers:
+def prepare_answer_snippet(answer):
+  rid, ans = answer
+
+  if "code" not in ans:
+      return None
+
   # Post contains an accepted answer
-  if "pre" in acceptedAnswers[rid]["code"]:                                
+  if "pre" in ans["code"]:                                
     # Title is good
-    #titleFilter = s.filter(acceptedAnswers[rid]['title'])           
+    #titleFilter = s.filter(ans['title'])           
     if True:#titleFilter == 0:
 
-      soup = BeautifulSoup(acceptedAnswers[rid]["code"], 'html.parser')
+      soup = BeautifulSoup(html.unescape(ans["code"]), 'html.parser')
       codeTag = soup.find_all('pre')
       # Contains exactly one piece of code
       if len(codeTag) == 1:                                         
@@ -64,23 +87,37 @@ for rid in acceptedAnswers:
             code[0:7].lower() == "select " or code[0:7].lower() == "update " or code[0:6].lower() == "alter " or \
             code[0:2].lower() == "c:" or code[0:4].lower() == "http" or code[0:4].lower() == "hkey" or \
                   re.match(r"^[a-zA-Z0-9_]*$", code) is not None:
-            pass
+            return None
           else:
             # Now also make sure it parses
             try:
-              code = code.replace('>>>','')
+              code = code.replace('>>>','').strip()
               ast.parse(code)
-
-              code_pairs.append({
-                  "question_id": rid,
-                  "parent_answer_post_id": acceptedAnswers[rid]['id'],
-                  "intent": acceptedAnswers[rid]['title'],
-                  "snippet": code})
+              #code = astor.dump_tree(ast.parse(code), maxline=99999, maxmerged=99999).strip()
+              lines = [l for l in code.strip().split('\n') if
+                          not re.match(r"^\s*(import|from|def|#)",l)
+                          and l.strip()]
+              if len(lines) == 1:
+                code = lines[0].strip()
+                return {
+                    "question_id": rid,
+                    "parent_answer_post_id": ans['id'],
+                    "intent": ans['title'],
+                    "snippet": code}
             except:
-              pass
+              print('parse fail')
+              return None
+        else:
+          return None
+      else:
+        return None
+
+print('starting processing')
+pool = Pool(processes=16)
+code_pairs = pool.map(prepare_answer_snippet, acceptedAnswers.items())
 
 with open(params["outputFile"], 'w') as f:
-  json.dump(code_pairs, f)
+  json.dump([p for p in code_pairs if p is not None], f)
 
 # Create training and validation and test sets
 #os.system('shuf python_all.txt > python_shuffled.txt')
